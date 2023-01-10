c66a86d463d45d78278e9e6017064b15bce9c22b
Stephen <skairu09@gmail.com>
2023-01-10 03:24:16 +0300

    Create profile, newreport components and routes

---
+++ src/Profile/profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className='bg-gray-200'>
+    <div className="bg-yellow-400 w-72 shadow-2xl text-lg align-self-center">
+    <p className='text-red-500'>Arma</p>
+    </div>
+    </div>
+  )
+}
+
+export default Profile
