2e976d7e6e59809c447aeb58d00bd97ba0505431
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-11 16:46:03 +0300

    this commit adds the individual posts to the project

---
+++ src/Components/my-posts.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function MyPosts() {
+    return(
+        <fragment>
+            <h3>My Posts</h3>
+            <form>
+            <input type='text' /> 
+            <input type="submit"/>
+            </form>
+            
+        </fragment>
+    )
+}
