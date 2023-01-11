5c1358e444204b7d5f4c453fccf09d68fda71fd4
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-11 16:50:34 +0300

    this commit adds the fetched data to the my posts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './Components/Home';
+import MyPosts from './Components/my-posts';
 
 function App() {
   return (
     <div className="App">
  <Home/>
+ <MyPosts />
     </div>
   );
 }
