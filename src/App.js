33774948403b15d7b7ece30fd181ad6f2e6477f8
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-12 21:36:19 +0300

    this commit merges the development to the map branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from './Profile/profile';
 import NewReport from './NewReport/NewReport';
 import Home from './Components/Home';
 import MyPosts from './Components/my-posts';
+import Maps from './Components/maps';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/newreport" element={<NewReport />} />
           <Route exact path='/home' element={<Home />} />
           <Route exact path='/myposts' element={<MyPosts />} />
+          <Route exact path='/map' element={<Maps />} />
       </Routes>
         {/* <Profile/> */}
         {/* <NewReport/> */}
