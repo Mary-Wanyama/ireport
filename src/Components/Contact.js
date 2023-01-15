97d4adcea95b54e6799f870bc10132492972acac
Felix <barosiofelix@gmail.com>
2023-01-15 09:42:05 +0300

    update: cont

---
+++ src/Components/Contact.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+import './Contact.css'
+
+const Contact = () => {
+
+    return (
+        <>
+            <section id="contact">
+                <h2>Contact Us</h2>
+                <h4 className='lorem'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</h4>
+                <div className='contact_container'>
+                    <div className="contact_options">
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Contact;
