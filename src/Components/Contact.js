e8c52fac7fa1ac59b3d7cd763c589dd7966acc40
Felix <barosiofelix@gmail.com>
2023-01-15 09:43:12 +0300

    add: contact-form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,22 @@
 import React, { useRef } from 'react';
 import './Contact.css'
 
+
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+
+        e.target.reset()
+    };
 
     return (
         <>
@@ -11,6 +26,13 @@ const Contact = () => {
                 <div className='contact_container'>
                     <div className="contact_options">
                     </div>
+
+                    <form className='contact_form' ref={form} onSubmit={sendEmail}>
+                        <input type="text" name="name" placeholder="Your Full Name" required />
+                        <input type="email" name="email" placeholder="Your Email" required />
+                        <textarea name="message" rows="7" placeholder='Your Message' required />
+                        <button type="submit" className='btn btn-primary'>Send Message</button>
+                    </form>
                 </div>
             </section>
         </>
