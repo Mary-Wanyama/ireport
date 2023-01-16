3a1e360d4ff8eb4ab0b65b886a6d6955733eef88
Stephen <skairu09@gmail.com>
2023-01-17 02:20:53 +0300

    Add report category in form

--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -6,13 +6,13 @@ function Sidebar() {
     return (
         <>
             <div className="sidebar-list text-white w-44 z-40 fixed h-full grid justify-items-center mb-4 shadow-2xl">
-                <div className='mt-3 mb-9 grid-flow-row-dense'><img className="rounded-full h-28 w-28 mx-auto" src="https://w7.pngwing.com/pngs/632/307/png-transparent-paper-planes-send-newsletter-icon-blue-tinker-thumbnail.png" alt=""/>
+                <div className='mt-3 mb-2 grid-flow-row-dense'><img className="rounded-full h-28 w-28 mx-auto" src="https://w7.pngwing.com/pngs/632/307/png-transparent-paper-planes-send-newsletter-icon-blue-tinker-thumbnail.png" alt=""/>
                 <h1 className='text-4xl mt-2 text-yellow-400 font-bold'>Chiriku</h1>
                 </div>
-                <div className='mb-1'>
+                <div className='mb-1 text-lg font-bold'>
                 {SidebarData.map((val, key)=>{
                         return(
-                            <div className='mb-6 hover:text-yellow-400' key={key}>
+                            <div className='mb-2 hover:text-yellow-400' key={key}>
                                 <NavLink to={val.path}
                                 className="row"
                                 id={window.location.pathname === val.path ? "active" : ""}
