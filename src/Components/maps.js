2cd2f33540468a7101ad8c1102695675d3d09016
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2023-01-17 12:46:58 +0300

    this commit removes some npm packages

--- src/Components/maps.js
+++ src/Components/maps.js
@@ -1,4 +1,3 @@
-import { IconButton} from "@chakra-ui/react";
 import { FaLocationArrow, FaTimes } from "react-icons/fa";
 import {useJsApiLoader, GoogleMap, Marker, OverlayView
 } from "@react-google-maps/api";
@@ -13,10 +12,9 @@ function Maps() {
     libraries: ["places"],
   });
   // const marks = [{ lat: -1.286389, lng: 36.817223 }, {lat: -4.043740, lng: 39.658871}, {lat: -0.717178, lng: 36.431026}]
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  // const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const url = "https://report-production-8d93.up.railway.app/alerts"
   const [pointer, setPointers] = useState([])
-  const [show, setShow] = useState([])
 
   useEffect(()=>{
     fetch(url)
@@ -32,15 +30,6 @@ function Maps() {
     return "hello";
   }
 
-//   function details(id){
-//     useEffect(()=>{
-//         fetch(url)
-//         .then(res=>res.json())
-//         .then(json =>{
-//             setPointers(json) 
-//         })
-//     }, [])
-//   }
 
   return (
     <div>
@@ -62,33 +51,8 @@ function Maps() {
             <Marker position={center} />
           </GoogleMap>
         </div>
-          <div className="map-center">
-            {/* <div className="map-hover">hello</div> */}
-            <IconButton
-            className="map-button"
-              icon={<FaLocationArrow />}
-              isRound
-              onClick={() => map.panTo(center)}
-            />
-          </div>
-          {/* <div className="center-map"> */}
-            {/* <div className="map-hover">hello</div> */}
-            {/* <IconButton
-            className="button-map"
-            {<Overlay />}
-              icon={<FaTimes />}
-              
-            /> */}
-          {/* </div> */}
-          <OverlayView position={center}>
-                <div>
-                    <h1>hello</h1>
-                </div>
-          </OverlayView> 
-          {/* <div className="map-hover">
-            <img src="https://images.unsplash.com/photo-1673845861099-2785734e247c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="image" /> 
-            <h3>title</h3>
-          </div> */}
+
+
 
       </div>
     </div>
